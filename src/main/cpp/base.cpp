d7f824cd177644d79da7fb1e34392a23668c93f8
Jere655 <jeremie.lavalliere@hotmail.com>
2026-05-21 07:12:48 -0400

    add pneumatic

--- src/main/cpp/base.cpp
+++ src/main/cpp/base.cpp
@@ -4,6 +4,7 @@ void Base::RobotInit() {
     wpi::SendableRegistry::AddChild(&m_robotDrive, &m_leftMotor);
     wpi::SendableRegistry::AddChild(&m_robotDrive, &m_rightMotor);
     m_rightMotor.SetInverted(true);
+    m_compressor.EnableDigital();
 }
 
 void Base::TeleopPeriodic() {
@@ -14,4 +15,19 @@ void Base::TeleopPeriodic() {
     if (std::abs(rotation) < 0.15) rotation = 0;
 
     m_robotDrive.ArcadeDrive(vitesse, rotation);
+
+    if (m_driverController.GetXButton()) {
+        m_intake.Set(frc::DoubleSolenoid::Value::kForward);
+    } else {
+        m_intake.Set(frc::DoubleSolenoid::Value::kReverse);
+    }
+
+    double rt = m_driverController.GetRightTriggerAxis();
+    if (rt > 0.1) {
+        m_intakeMotor.Set(rt);
+    } else if (m_driverController.GetXButton()) {
+        m_intakeMotor.Set(-0.5);
+    } else {
+        m_intakeMotor.Set(0);
+    }
 }
--- src/main/include/base.h
+++ src/main/include/base.h
@@ -3,6 +3,10 @@
 #include <CANVenom.h>
 #include <frc/XboxController.h>
 #include <frc/drive/DifferentialDrive.h>
+#include <frc/DoubleSolenoid.h>
+#include <frc/PneumaticsModuleType.h>
+#include <frc/Compressor.h>
+#include <rev/SparkMax.h>
 
 class Base {
 public:
@@ -14,4 +18,7 @@ private:
     pwf::CANVenom m_rightMotor{2};
     frc::XboxController m_driverController{0};
     frc::DifferentialDrive m_robotDrive{m_leftMotor, m_rightMotor};
+    frc::DoubleSolenoid m_intake{3, frc::PneumaticsModuleType::CTREPCM, 0, 1};
+    frc::Compressor m_compressor{3, frc::PneumaticsModuleType::CTREPCM};
+    rev::spark::SparkMax m_intakeMotor{17, rev::spark::SparkMax::MotorType::kBrushless};
 };
